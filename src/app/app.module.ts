7c394b46cf1f03ab187ee94ddfea2a80d1f8cc52
jsongo <jsongo@qq.com>
2018-07-08 17:04:55 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+    MatToolbarModule, MatButtonModule, MatSidenavModule,
+    MatIconModule, MatListModule, MatGridListModule,
+    MatCardModule, MatMenuModule, MatTreeModule
+} from '@angular/material';
+import { MainDashComponent } from './main-dash/main-dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MainDashComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
